6d935ead7418af7cc9c98534394c248e959e3ccf
Eduardo Kiriakos <32148180+Edkiri@users.noreply.github.com>
2022-01-26 16:00:56 -0400

    Video listo

--- index.js
+++ index.js
@@ -23,3 +23,40 @@ buttons.forEach(btn => {
     }
   })
 })
+
+const playBtn = document.querySelector('#playBtn');
+const pauseBtn = document.querySelector('#pauseBtn');
+const videoContainer = document.getElementById('video');
+
+const carouselButtons = document.querySelectorAll('.carousel-menu li');
+carouselButtons.forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    const activeBtn = document.querySelector('.carousel-menu li.active');
+    if(event.target !== activeBtn) {
+      activeBtn.classList.remove('active');
+      event.target.classList.add('active');
+      videoContainer.style.opacity = 0;
+      playBtn.style.display = 'block';
+      pauseBtn.style.display = 'none';
+      const liIdToShow = btn.id.split("-")[1];
+      const liToHide = document.querySelector(`#${activeBtn.id.split("-")[1]}`);
+      liToHide.style.opacity = 0;
+      const liToshow = document.querySelector(`#${liIdToShow}`);
+      liToshow.style.opacity = 1;
+    }
+  })
+  document.querySelector('#person1').style.opacity = 1
+})
+
+playBtn.addEventListener('click', event => {
+  videoContainer.style.opacity = 1;
+  playBtn.style.display = 'none';
+  pauseBtn.style.display = 'block';
+  videoContainer.children[0].play();
+}); 
+
+pauseBtn.addEventListener('click', event => {
+  videoContainer.children[0].pause();
+  playBtn.style.display = 'block';
+  pauseBtn.style.display = 'none';
+})
